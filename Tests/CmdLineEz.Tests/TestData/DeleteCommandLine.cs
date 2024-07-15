fbe1bdf971377957759b4589eb498f8136ce69a0
Ihor Pankiv <ihorpankiv.dev@gmail.com>
2024-07-15 16:05:47 +0300

    Added DeleteCommandLine

---
+++ Tests/CmdLineEz.Tests/TestData/DeleteCommandLine.cs
@@ -0,0 +1,20 @@
+﻿using CmdLineEz.Attributes;
+
+namespace CmdLineEz.Tests.TestData
+{
+    public class DeleteCommandLine
+    {
+        [CmdLineEz("confirm", CmdLineEzAttributeFlags.Required)]
+        public bool ConfirmNeeded { get; set; }
+
+        public bool Recursive { get; set; }
+
+        [CmdLineEz("verbose")]
+        public bool PrintDetails { get; set; }
+
+        public string Prefix { get; set; }
+
+        [CmdLineEz("verbose", CmdLineEzAttributeFlags.Remaining)]
+        public List<string> Remaining {get; set;}
+    }
+}
