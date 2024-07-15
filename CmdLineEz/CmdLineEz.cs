82dbad6fc9481c3b2c8ff7ca5921dd07e54c01a0
Ihor Pankiv <ihorpankiv.dev@gmail.com>
2024-07-15 14:54:01 +0300

    Implemented reflection-part of CmdLineEz<T>.Process

--- CmdLineEz/CmdLineEz.cs
+++ CmdLineEz/CmdLineEz.cs
@@ -1,4 +1,7 @@
-﻿namespace CmdLineEz
+﻿using CmdLineEz.Attributes;
+using System.Reflection;
+
+namespace CmdLineEz
 {
     public class CmdLineEz<T>
     {
@@ -11,7 +14,58 @@
         /// <exception cref="NotSupportedException">Throws if </exception>
         public static List<string> Process(T result, string[] args)
         {
-            throw new NotImplementedException();
+            var errors = new List<string>();
+            var argsList = args.ToList();
+
+            var properties = typeof(T).GetProperties();
+            var remainingProperty = properties
+                .Where(p => p.GetCustomAttribute<CmdLineEzAttribute>()?.Flags.HasFlag(CmdLineEzAttributeFlags.Remaining) == true);
+
+            foreach (var prop in properties)
+            {
+                var attribute = prop.GetCustomAttribute<CmdLineEzAttribute>();
+                string paramName = attribute?.AltName ?? prop.Name.ToLower();
+
+                var inputParam = argsList.FirstOrDefault(a =>
+                    a.StartsWith($"/{paramName}=", StringComparison.OrdinalIgnoreCase));
+
+                if (inputParam != null)
+                {
+                    string value = paramName.Split('=')[1];
+
+                    try
+                    {
+                        prop.SetValue(result, prop.PropertyType.Name switch
+                        {
+                            nameof(Boolean) => true,
+                            nameof(Int32) => int.Parse(value),
+                            nameof(Decimal) => decimal.Parse(value),
+                            nameof(String) => value,
+                            _ => throw new NotSupportedException($"Type {prop.PropertyType} in not supported")
+                        });
+                    }
+                    catch (Exception)
+                    {
+                        errors.Add($"invalid {paramName}");
+                    }
+                }
+                else if (attribute?.Flags.HasFlag(CmdLineEzAttributeFlags.Required) == true)
+                {
+                    errors.Add($"missing {paramName}");
+                }
+            }
+
+            if (remainingProperty.Count() > 1)
+            {
+                throw new InvalidOperationException("There are not allowed more then 1 remaining properties.");
+            }
+            else if (remainingProperty.Count() == 1)
+            {
+                List<string> remainingArgs = args.Where(a => !a.StartsWith("/")).ToList();
+                remainingProperty.First().SetValue(result, remainingArgs);
+            }
+
+            return errors.Any() ? errors : null!;
         }
     }
 }
