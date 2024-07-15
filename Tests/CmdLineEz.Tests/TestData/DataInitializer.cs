4d0e5e170503fe50a9524bd70d6890ecd9673c69
Ihor Pankiv <ihorpankiv.dev@gmail.com>
2024-07-15 16:29:14 +0300

    Added DataInitializer

---
+++ Tests/CmdLineEz.Tests/TestData/DataInitializer.cs
@@ -0,0 +1,16 @@
+﻿using NSubstitute;
+
+namespace CmdLineEz.Tests.TestData
+{
+    public class DataInitializer
+    {
+        public static IEnumerable<TestCaseData> GetNullInsteadOfDeleteCommandLine()
+        {
+            yield return new TestCaseData(null, null);
+            yield return new TestCaseData(null, new string[] { });
+            yield return new TestCaseData(null, new string[3] { "Example", null!, string.Empty });
+            yield return new TestCaseData(null, new string[5] { "", "", "", "", "" });
+
+        }
+    }
+}
