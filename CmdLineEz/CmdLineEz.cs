8d73290e256d63c1b016c23aabd9f112b26aa0b5
Ihor Pankiv <ihorpankiv.dev@gmail.com>
2024-07-15 14:51:45 +0300

    Add project files.

---
+++ CmdLineEz/CmdLineEz.cs
@@ -0,0 +1,17 @@
+﻿namespace CmdLineEz
+{
+    public class CmdLineEz<T>
+    {
+        /// <summary>
+        /// Pass your customized template class here to read the fields, pass the args array into this to process it and set the values.
+        /// </summary>
+        /// <param name="result">Your customized template class</param>
+        /// <param name="args">Array of args, generally speaking pass in args from the Main function</param>
+        /// <returns>Collection of string-errors or null if there are no error</returns>
+        /// <exception cref="NotSupportedException">Throws if </exception>
+        public static List<string> Process(T result, string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
